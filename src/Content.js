44dc58427ed54e7cdcc8cd0705599233d0c9ca5d
harikrishnan-13 <135119257+harikrishnan-13@users.noreply.github.com>
2023-12-20 21:58:06 +0530

    handle functions created

--- src/Content.js
+++ src/Content.js
@@ -3,45 +3,64 @@ import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
 
-    const [items,setItems]= useState([
+    const [items, setItems] = useState([
         {
-            id:1,
+            id: 1,
             checked: true,
-            item:"play football"
+            item: "play football"
         },
         {
-            id:2,
+            id: 2,
             checked: true,
-            item:"do exercise"
+            item: "do exercise"
         },
         {
-            id:3,
+            id: 3,
             checked: true,
-            item:"read books"
+            item: "read books"
         },
         {
-            id:4,
+            id: 4,
             checked: true,
-            item:"do coding"
+            item: "do coding"
         }
     ])
 
+    const handleChange = (id) => {
+        const listItems = items.map((item) =>
+            item.id === id ? { ...item, checked: !item.checked } : item)
+        setItems(listItems);
+        localStorage.setItem("todo_list", JSON.stringify(listItems))
+    }
+
+    const handledelete = (id) => {
+        const updatedItems = items.filter((item) => item.id !== id);
+        setItems(updatedItems);
+        localStorage.setItem("todo_list", JSON.stringify(updatedItems))
+    }
+
     return (
         <main>
-                <ul>
-                    {items.map((item)=>(
+            {items.length ?
+                (<ul>
+                    {items.map((item) => (
                         <li className='item' key={item.id}>
                             <input
-                            type='checkbox'
-                            checked={item.checked} >
+                                type='checkbox'
+                                onChange={() => handleChange(item.id)}
+                                checked={item.checked} >
                             </input>
-                            <label>{item.item}</label>
-                            <FaTrashAlt 
-                            role="button"
-                            tabIndex="0"/>
+                            <label
+                                style={(item.checked) ? { textDecoration: 'line-through' } : null}
+                                onDoubleClick={() => handleChange(item.id)}>{item.item}</label>
+                            <FaTrashAlt
+                                role="button"
+                                tabIndex="0"
+                                onClick={() => handledelete(item.id)} />
                         </li>
-                        ))}
-                </ul>
+                    ))}
+                </ul>) : (<p style={{marginTop:'2rem'}}>your list is empty...</p>)
+            }
         </main>
     )
 }
